08927a6e8dea1c78a756c20112cc6af1c38996ca
louishsu <is.louishsu@foxmail.com>
2019-11-30 19:26:58 +0800

    first commit

---
+++ models/backbone.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+'''
+@Description: 
+@Version: 1.0.0
+@Author: louishsu
+@Github: https://github.com/isLouisHsu
+@E-mail: is.louishsu@foxmail.com
+@Date: 2019-11-30 15:20:57
+@LastEditTime: 2019-11-30 18:15:33
+@Update: 
+'''
+import torch
+from torch import nn
+from torchvision import models
+
+from module import conv1x1, conv3x3, Bottleneck
+
+class AlexNet(nn.Module):
+    configs = [3, 96, 256, 384, 384, 256]
+
+    def __init__(self, width_mult=1):
+        configs = list(map(lambda x: 3 if x == 3 else
+                       int(x*width_mult), AlexNet.configs))
+        super(AlexNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(configs[0], configs[1], kernel_size=11, stride=2),
+            nn.BatchNorm2d(configs[1]),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(configs[1], configs[2], kernel_size=5),
+            nn.BatchNorm2d(configs[2]),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(configs[2], configs[3], kernel_size=3),
+            nn.BatchNorm2d(configs[3]),
+            nn.ReLU(inplace=True),
+            )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(configs[3], configs[4], kernel_size=3),
+            nn.BatchNorm2d(configs[4]),
+            nn.ReLU(inplace=True),
+            )
+
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(configs[4], configs[5], kernel_size=3),
+            nn.BatchNorm2d(configs[5]),
+            )
+        self.feature_size = configs[5]
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        return x
+
+class ResNet(nn.Module):
+    
+    def __init__(self, block, layers, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=0,    # conv7x7, s2, p3 -> conv7x7, s2, p0
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)                 # maxpool3x3, s2, p1 -> maxpool2x2, s2, p0 
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        # self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+        #                                dilate=replace_stride_with_dilation[2])
+
+        self.feature_size = [ch * block.expansion for ch in [64, 128, 256]]
+
+        self._load_pretrain()
+
+    def _load_pretrain(self):
+
+        pretrain = models.resnet50(pretrained=True).state_dict()
+        totrain = self.state_dict()
+        update = {k: v for k, v in pretrain.items() if k in totrain.keys()}
+        totrain.update(update)
+        self.load_state_dict(totrain)
+        
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        f1 = self.layer1(x)
+        f2 = self.layer2(f1)
+        f3 = self.layer3(f2)
+        # f4 = self.layer4(f3)
+
+        return f1, f2, f3
+
+def resnet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+if __name__ == "__main__":
+
+    from torchstat import stat
+    from tensorboardX import SummaryWriter
+
+    dsize = [3, 127, 127]
+    
+    # m = ResNet(Bottleneck, [3, 4, 6, 3])
+    m = AlexNet()
+    x = torch.rand(1, *dsize)
+
+    stat(m, dsize)
+    # with SummaryWriter() as w:
+    #     w.add_graph(m, x)
+
---
+++ models/head.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+'''
+@Description: 
+@Version: 1.0.0
+@Author: louishsu
+@Github: https://github.com/isLouisHsu
+@E-mail: is.louishsu@foxmail.com
+@Date: 2019-11-30 15:28:34
+@LastEditTime: 2019-11-30 19:10:50
+@Update: 
+'''
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class RpnHead(nn.Module):
+    """ SiamRPN head """
+
+    def __init__(self, in_channels, out_channels=256, num_anchor=5):
+        super(RpnHead, self).__init__()
+    
+        self.template_cls = nn.Conv2d(in_channels, out_channels * num_anchor,     kernel_size=3)
+        self.template_reg = nn.Conv2d(in_channels, out_channels * num_anchor * 4, kernel_size=3)
+
+        self.search_cls   = nn.Conv2d(in_channels, out_channels, kernel_size=3)
+        self.search_reg   = nn.Conv2d(in_channels, out_channels, kernel_size=3)
+
+        self.adjust = nn.Conv2d(num_anchor * 4, num_anchor * 4, kernel_size=1)
+
+    def _conv(self, f, k):
+        """
+        Params:
+            f: {tensor(N, ch, Hf, Wf)} 
+            k: {tensor(N, ch * k, Hk, Wk)} 
+        Returns:
+            po: {tensor(N, k, H, W)}
+        """
+        n, ch = f.size()[:2]
+        
+        px = f.view( 1, -1, *f.size()[2:])      # (    1, N * ch, Hf, Wf)
+        pk = k.view(-1, ch, *k.size()[2:])      # (N * k,     ch, Hk, Wk)
+        po = F.conv2d(px, pk, groups=n)         # (    1, N *  k,  H,  W)
+        po = po.view(n, -1, *po.size()[2:])     # (    N,      k,  H,  W)
+        return po
+
+    def forward(self, z_f, x_f):
+        """
+        Params:
+            z_f: {tensor(N, in_channels, Hz, Wz)} feature extracted from template
+            x_f: {tensor(N, in_channels, Hx, Wx)} feature extracted from search
+        Returns:
+            pred_cls: {tensor(N, num_anchor,     H, W)}
+            pred_reg: {tensor(N, num_anchor * 4, H, W)}
+        """
+        cls_feature = self.search_cls(x_f)      # (N, out_channels, Hx', Wx')
+        reg_feature = self.search_reg(x_f)      # (N, out_channels, Hx', Wx')
+
+        cls_kernel = self.template_cls(z_f)     # (N, out_channels * num_anchor,     Hz', Wz')
+        reg_kernel = self.template_reg(z_f)     # (N, out_channels * num_anchor * 4, Hz', Wz')
+
+        cls_kernel = cls_kernel.view(-1, cls_feature.size(1), *cls_kernel.size()[2:])   # (N, out_channels * num_anchor,     Hz', Wz')
+        reg_kernel = reg_kernel.view(-1, reg_feature.size(1), *reg_kernel.size()[2:])   # (N, out_channels * num_anchor * 4, Hz', Wz')
+        
+        pred_cls = self._conv(cls_feature, cls_kernel)              # (N, num_anchor, H, W)
+        pred_reg = self.adjust(self._conv(reg_feature, reg_kernel)) # (N, num_anchor * 4, H, W)
+
+        return pred_cls, pred_reg
+                
+
+class HeatmapHead(nn.Module):
+    """ SiamAF head """
+    def __init__(self, in_channels):
+        super(HeatmapHead, self).__init__()
+        
+        self.heat = nn.Conv2d(in_channels, 1, kernel_size=3)
+        self.reg  = nn.Conv2d(in_channels, 4, kernel_size=3)
+        self.adjust = nn.Conv2d(4, 4, kernel_size=1)
+
+    def _conv(self, f, k):
+        """
+        Params:
+            f: {tensor(N, ch, Hf, Wf)} 
+            k: {tensor(N, ch, Hk, Wk)} 
+        Returns:
+            po: {tensor(N, ch, H, W)}
+        """
+        n, ch = f.size()[:2]
+        
+        px = f.view( 1, -1, *f.size()[2:])      # (     1, N * ch, Hf, Wf)
+        pk = k.view(-1,  1, *k.size()[2:])      # (N * ch,      1, Hk, Wk)
+        po = F.conv2d(px, pk, groups=n*ch)      # (     1, N * ch,  H,  W)
+        po = po.view(n, -1, *po.size()[2:])     # (     N,     ch,  H,  W)
+        return po
+
+    def forward(self, z_f, x_f):
+        """
+        Params:
+            z_f: {tensor(N, in_channels, Hz, Wz)} feature extracted from template
+            x_f: {tensor(N, in_channels, Hx, Wx)} feature extracted from search
+        Returns:
+            pred_map: {tensor(N, 1, H, W)}
+            pred_reg: {tensor(N, 4, H, W)}
+        """
+        corr = self._conv(x_f, z_f)
+        pred_heat = self.heat(corr)
+        pred_reg  = self.reg (corr)
+
+        return pred_heat, pred_reg
+        
+
+# if __name__ == "__main__":
+    
+#     in_channels = 256
+#     # m = RpnHead(in_channels)
+#     m = AfHead(in_channels)
+#     z = torch.rand(5, in_channels,  6,  6)
+#     x = torch.rand(5, in_channels, 22, 22)
+#     m(z, x)
