611bb46b3fa94b8f3c15641a21536a9c6363e977
louishsu <is.louishsu@foxmail.com>
2019-12-13 11:59:48 +0800

    Time: 2019-12-13 11:59:48

---
+++ script/train_siamrpn.sh
@@ -0,0 +1,13 @@
+#! bin/bash
+### 
+# @Description: 
+ # @Version: 1.0.0
+ # @Author: louishsu
+ # @E-mail: is.louishsu@foxmail.com
+ # @Date: 2019-12-13 11:58:11
+ # @LastEditTime: 2019-12-13 11:58:59
+ # @Update: 
+ ###
+cd ../main
+rm -rf ../logs/siamrpn/*
+python siamrpn.py
